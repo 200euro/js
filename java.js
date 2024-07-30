e7e073fdbd3960c9352ca0916146f75fd151519a
200euro <AKMALKADYROV281@GMAIL.COM>
2024-07-30 16:46:59 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,72 @@
+
+// function sayHello(name){
+// console.log('salom ' + name);
+// }
+
+// sayHello('Abbos')
+
+
+// declaration
+
+// sum(10, 5)
+
+// sum('asd', 13)
+
+// sum(2, 'qwerty')
+
+// sum(23, 1)
+
+// sum('Abbos', 'Valiev' )
+
+// sum('b', 10)
+// sum( 0, 1 , 2)
+
+// sum(1,2)
+// (можно вызывать до его создания)
+// function sum(a, b,) {
+//    console.log(a + b );
+// }
+
+
+
+
+// const plus = (a, b) => a + b
+// const minus = ( a, b) => a - b
+// const umnoj = ( a, b) => a * b  
+// const delen =  (a, b) => a / b
+
+// console.log(minus(5, 5)); 
+// console.log(delen(5, 5));
+// console.log(plus(5, 5));
+// console.log(umnoj(5, 5));
+
+
+
+// expression
+//(нельзя вызвать до создания)
+// const greet = function(asd) {
+//    console.log('qalesan ' + asd );
+
+// }
+
+
+// sum (5, 10)
+// greet('Eldor')
+// greet ('Otabek')
+
+
+// const me = function (name, surname, age) {
+//    console.log('Привет меня зовут ' + name + ' ' + surname + ' мне ' + age + ' ' + 'лет');
+// }
+
+// me('Akmal', 'Kadirov' , 16 )
+
+
+
+const me = function (name, surname, age) {
+  console.log(`Привет меня зовут ${name} ${surname}, мне ${age} лет`  );
+}
+
+
+
+me('Акмал', 'Кадыров' , 16 )
